95e3c287882245db218eef14a63f7f2fe394bf88
yutaro76 <yutaro.okuda.76@gmail.com>
2022-04-23 12:42:32 +0000

    CRUD messages

---
+++ resources/views/messages/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>メッセージ一覧</h1>
+    
+    @if (count($messages) > 0)
+        <table class="table table-striped">
+            <thread>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thread>
+            <tbody>
+                @foreach ($messages as $message)
+                <tr>
+                    <td>{!! link_to_route('messages.show', $message->id, ['message' => $message->id]) !!}</td>
+                    <td>{{ $message->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {{--　メッセージ作成ページへのリンク　--}}
+    {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesController@index');
+Route::resource('messages', 'MessagesController');
+
+// CRUD
+// メッセージの個別詳細ページ表示
+// Route::get('messages/{id}', 'MessagesController@show');
+// メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
+// Route::post('messages', 'MessagesController@store');
+// メッセージの更新処理（編集画面を表示するためのものではありません）
+// Route::put('messages/{id}', 'MessagesController@update');
+// メッセージを削除
+// Route::delete('messages/{id}', 'MessagesController@destroy');
+
+// index: showの補助ページ　一覧ページ
+// Route::get('messages', 'MessagesController@index')->name('messages.index');
+// create: 新規作成用のフォームページ　新規登録画面
+// Route::get('messages/create', 'MessagesController@create')->name('messages.create');
+// edit: 更新用のフォームページ
+// Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit');
+
